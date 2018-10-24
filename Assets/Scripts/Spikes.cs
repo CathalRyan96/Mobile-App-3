54789dd26c8e1c0118883a40d47bbdb747ad9cce
CathalRyan96 <cathalryan48@gmail.com>
2018-10-24 12:21:48 +0100

    attempting to get spikes to work

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    private PlayerController player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+
+        }
+    }
+}
