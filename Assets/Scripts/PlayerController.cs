ba85751e283bd3918a7316f7a637692ca705e711
CathalRyan96 <cathalryan48@gmail.com>
2018-10-29 20:06:33 +0000

    spikes functionality now working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -32,5 +33,15 @@ public class PlayerController : MonoBehaviour {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
         }
 
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "spike")
+        {
+            Scene scene;
+            scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
     }
 }
