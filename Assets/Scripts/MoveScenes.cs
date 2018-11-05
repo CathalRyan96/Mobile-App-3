aff1a7842ea6c8cb7ccc96a16336e916f6cf7a94
CathalRyan96 <cathalryan48@gmail.com>
2018-11-05 14:02:10 +0000

    attempting to get checkpoint to work

---
+++ Assets/Scripts/MoveScenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScenes : MonoBehaviour {
+    [SerializeField] private string newLevel;
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("player"))
+        {
+            SceneManager.LoadScene(newLevel);
+        }
+    }
+
+	
+}
