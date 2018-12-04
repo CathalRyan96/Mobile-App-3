47d37485ad671b0187c461fd151c24dcc041f8a3
CathalRyan96 <cathalryan48@gmail.com>
2018-12-04 12:28:27 +0000

    return to main menu button added

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -18,6 +19,11 @@ public class PauseMenu : MonoBehaviour {
         Pausemenu.SetActive(false);
         PauseButton.SetActive(true);
         Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
     }
 
 }
