33102105dbdaf32725c9978283e2e09a58fc46c6
CathalRyan96 <cathalryan48@gmail.com>
2018-12-05 16:43:37 +0000

    trying to add double jump ability

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,18 +11,30 @@ public class PlayerController : MonoBehaviour {
 
     public Rigidbody2D rb;
 
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask grounded;
+    private int doubleJump;
+    public int doubleJumpValue;
+
     
 
     void Start()
     {
+        doubleJump = doubleJumpValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
 
-        
+        if(isGrounded == true)
+        {
+            doubleJump = doubleJumpValue;
+        }
 
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, grounded);
         //player movement
         moveInput = Input.GetAxis("Horizontal");
         Debug.Log(moveInput);
@@ -32,6 +44,16 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && doubleJump > 0)
+        {
+            rb.velocity = Vector2.up * jumpPower;
+            doubleJump--;
+        }else if(Input.GetKeyDown(KeyCode.Space) && doubleJump == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpPower;
+
+
         }
 
     }
