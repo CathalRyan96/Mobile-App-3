71c76c1dfcc2a26e292eafa71064f1d31bbd02c8
CathalRyan96 <cathalryan48@gmail.com>
2018-12-06 11:49:54 +0000

    edited jump ability

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,64 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
     public float speed;
-    public float jumpPower;
+    public float jump;
     private float moveInput;
-
     public Rigidbody2D rb;
 
-    private bool isGrounded;
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask grounded;
-    private int doubleJump;
-    public int doubleJumpValue;
-
-    
+    public KeyCode jumpKey;
+    public string midJump = "n";
 
     void Start()
     {
-        doubleJump = doubleJumpValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-
-        if(isGrounded == true)
-        {
-            doubleJump = doubleJumpValue;
-        }
-
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, grounded);
         //player movement
         moveInput = Input.GetAxis("Horizontal");
-        Debug.Log(moveInput);
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
         //jumping
-        if (Input.GetKey(KeyCode.Space))
+        if ((Input.GetKeyDown(jumpKey)) && (midJump == "n"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && doubleJump > 0)
-        {
-            rb.velocity = Vector2.up * jumpPower;
-            doubleJump--;
-        }else if(Input.GetKeyDown(KeyCode.Space) && doubleJump == 0 && isGrounded == true){
-            rb.velocity = Vector2.up * jumpPower;
-
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 5, 0);
+            midJump = "y";
 
         }
 
+        if (GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            midJump = "n";
+
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.tag == "spike")
+        if (col.tag == "spike")
         {
             Scene scene;
             scene = SceneManager.GetActiveScene();
@@ -68,3 +49,8 @@ public class PlayerController : MonoBehaviour {
         }
     }
 }
+
+
+
+    
+
